d22cf4cafe5eea433a890d3f31718b66f2b0d788
minyangwr <minyang3194@live.com>
2024-08-16 04:42:14 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -73,24 +73,24 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 git clone --single-branch --depth 1 -b master https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
 
 # 预置Clash内核
-echo -e "预置Clash内核"
-mkdir -p package/luci-app-openclash/root/etc/openclash/core
-core_path="package/luci-app-openclash/root/etc/openclash/core"
-goe_path="package/luci-app-openclash/root/etc/openclash"
+#echo -e "预置Clash内核"
+#mkdir -p package/luci-app-openclash/root/etc/openclash/core
+#core_path="package/luci-app-openclash/root/etc/openclash/core"
+#goe_path="package/luci-app-openclash/root/etc/openclash"
 
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep "amd64" | awk -F '"' '{print $4}' | grep "v3" )
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+#CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+#CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep "amd64" | awk -F '"' '{print $4}' | grep "v3" )
+#CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+#GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+#GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
-wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
-wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+#wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+#wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+#wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
+#wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
+#wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
 
-chmod +x $core_path/clash*
+#chmod +x $core_path/clash*
 
 # socat
 cp -rf ../Lienol_pkg/luci-app-socat ./package/new/luci-app-socat
